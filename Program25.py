607b1fae9f64592c66ee5394cc88f5f74a92a00c
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-04 12:23:34 +0530

    get recursion time

---
+++ Program25.py
@@ -0,0 +1,5 @@
+# Write a Python program to get the current value of the recursion limit.
+
+import sys
+#return max depth of python interpreter stack
+print("Current value of the recursion limit:",sys.getrecursionlimit())
