962acf0a188f8f0496de5dcd381c849c67566902
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-04 12:08:55 +0530

    __doc__

---
+++ Program4.py
@@ -0,0 +1,8 @@
+# Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+# Sample function : abs()
+# Expected Result :
+# abs(number) -> number
+# Return the absolute value of the argument.
+
+# prints short documentation of its feature
+print(len.__doc__)
