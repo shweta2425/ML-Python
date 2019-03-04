6926dfc4a054c1a2bc8c72361cfb45ee36d597c7
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-04 12:34:34 +0530

    extract val of directory

---
+++ Program40.py
@@ -0,0 +1,12 @@
+# Write a Python program to extract single key-value pair of a dictionary in variables
+
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"GOOGLE"}
+
+print("printing Employee data .... ")
+
+print("Name : %s" %Employee["Name"])
+print("Age : %d" %Employee["Age"])
+print("Salary : %d" %Employee["salary"])
+print("Company : %s" %Employee["Company"])
+
+print(Employee["Name"])
