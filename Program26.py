e74c200e824c57bc5edb8c1dcd264f98468e3282
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-04 12:24:19 +0530

    occurrence of char

---
+++ Program26.py
@@ -0,0 +1,6 @@
+# Write a Python program to count the number occurrence of a specific character in a string.
+
+str='BridgeLabz Solution'
+sub=input("enter substr")
+#print occurrence of sub string
+print("occurence of",sub,"is",str.count(sub),"times")
