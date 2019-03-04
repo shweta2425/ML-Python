6302bc01e132b672cb1e688f371d3c2f36007add
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-04 12:23:08 +0530

    get size of obj

---
+++ Program24.py
@@ -0,0 +1,7 @@
+# Write a Python program to get the size of an object in bytes.
+
+import sys
+
+str1="shweta"
+#print size of the str in bytes
+print("memory size of",str1,"is ",sys.getsizeof(str1),"bytes")
