c9087ddf1a47091dfaa6b2578396e365a07a251b
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-11 11:15:07 +0530

    count occurrence in str

--- Week2/Week2/String2.py
+++ Week2/Week2/String2.py
@@ -1,6 +1,7 @@
 # 2. Write a Python program to count the number of characters (character frequency) in a string.
 # 	Sample String : google.com
 # 	Expected Result : {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+
 from Week2.Utilities import utility
 
 
