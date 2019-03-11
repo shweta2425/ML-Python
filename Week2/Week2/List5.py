eac34a128ffe6319b6cd5914b1f5ad1139a59a90
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-11 11:31:11 +0530

    sorted in increasing order by the last ele

--- Week2/Week2/List5.py
+++ Week2/Week2/List5.py
@@ -2,6 +2,7 @@
 # in each tuple from a given list of non-empty tuples.
 # Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 # Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
 from Week2.Utilities import utility
 
 
