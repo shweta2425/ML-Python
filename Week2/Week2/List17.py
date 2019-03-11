d2808ee4403eff6d7f19ac46459986d21561eb74
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-11 11:39:29 +0530

    remove duplicates from a list of lists

--- Week2/Week2/List17.py
+++ Week2/Week2/List17.py
@@ -1,6 +1,7 @@
 # Write a Python program to remove duplicates from a list of lists.
 # 	Sample list : [[10, 20], [40], [30, 56, 25], [10, 20], [33], [40]]
 # 	New List : [[10, 20], [30, 56, 25], [33], [40]]
+
 from Week2.Utilities import utility
 
 
