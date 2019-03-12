48de4551ed183000a233a5d55307775c03225c3b
shweta2425 <47652156+shweta2425@users.noreply.github.com>
2019-03-12 10:14:21 +0530

    Update Array1.py

--- Week2/Week2/Array1.py
+++ Week2/Week2/Array1.py
@@ -29,23 +29,3 @@ class Array1:
 
 obj = Array1()
 obj.disp()
-
-# arr = array('i', [10, 20, 30, 40, 50])
-# print(arr[0])
-# print(arr[1])
-# print(arr[2])
-# print(arr[3])
-# print(arr[4])
-# print(arr)
-
-
-# arr1 = array('i', [])
-#
-# NoOfEle = int(input("enter no of elements"))
-#
-# for i in range(NoOfEle):
-#     ele = int(input("enter element"))
-#     arr1.append(ele)
-# 
-# for i in range(NoOfEle):
-#     print(arr1[i])
